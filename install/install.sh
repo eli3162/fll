a5f7721f0b4ca4b004c53402feadbf6bbe3eb044
Ethan Li <fourdimentional31@gmail.com>
2026-02-25 21:36:08 -0500

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -14,7 +14,7 @@ wget https://artifact-alliance.vercel.app/install/GUI.png
 wget https://artifact-alliance.vercel.app/install/title.png
 cd
 sudo apt install figlet
-pip install -r requirements.txt
+pip install -r requirements.txt --break-system-packages
 figlet "ANCIENT VISION"
 echo "Installation complete! Reboot to start web server or run python service.py to start it now. The dashboard is hosted at http://localhost:1234"
 (crontab -l 2>/dev/null; echo "@reboot /home/ancient_vision_root/service.sh") | crontab -
