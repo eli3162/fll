a57b4fbae5f99f1ed1344ddbb24b1487d79cf743
Ethan Li <fourdimentional31@gmail.com>
2026-02-26 19:10:33 -0500

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 echo "MIT 2.0 License"
 cd
-wget https://artifact-alliance.vercel.app/install/vision.py
-wget https://artifact-alliance.vercel.app/install/DRV8835.py
-wget https://artifact-alliance.vercel.app/install/service.py
-wget https://artifact-alliance.vercel.app/install/stop.py
-wget https://artifact-alliance.vercel.app/install/service.sh
-wget https://artifact-alliance.vercel.app/install/requierments.txt
+wget https://artifact-alliance.vercel.app/install/vision.py -nv
+wget https://artifact-alliance.vercel.app/install/DRV8835.py -nv
+wget https://artifact-alliance.vercel.app/install/service.py -nv
+wget https://artifact-alliance.vercel.app/install/stop.py -nv
+wget https://artifact-alliance.vercel.app/install/service.sh -nv
+wget https://artifact-alliance.vercel.app/install/requierments.txt -nv
 chmod +x service.sh
 mkdir images
 cd images
-wget https://artifact-alliance.vercel.app/install/click.mp3
-wget https://artifact-alliance.vercel.app/install/button.png
-wget https://artifact-alliance.vercel.app/install/GUI.png
-wget https://artifact-alliance.vercel.app/install/title.png
+wget https://artifact-alliance.vercel.app/install/click.mp3 -nv
+wget https://artifact-alliance.vercel.app/install/button.png -nv
+wget https://artifact-alliance.vercel.app/install/GUI.png -nv
+wget https://artifact-alliance.vercel.app/install/title.png -nv
 cd
-sudo apt install figlet
+sudo apt install figlet -y -qq
 pip install -r requierments.txt --break-system-packages
 figlet "- ANCIENT VISION -"
 echo "Installation complete! Run python service.py to start it now. The dashboard is hosted at http://localhost:1234"
