75c510d29f4aa82dd9a1d5afbbd10946ba34104c
Ethan Li <fourdimentional31@gmail.com>
2026-02-26 19:14:38 -0500

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -18,7 +18,6 @@ wget https://artifact-alliance.vercel.app/install/button.png -nv
 wget https://artifact-alliance.vercel.app/install/GUI.png -nv
 wget https://artifact-alliance.vercel.app/install/title.png -nv
 cd
-sudo apt install figlet -y -qq
 pip install -r requierments.txt --break-system-packages -qq
 echo "Installation complete! Run python service.py to start it now. The dashboard is hosted at http://localhost:1234"
 echo "Also, set service.sh to run on startup!"
