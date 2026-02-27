79857c1e2f1dcd31aa32dd5e8e30ecb726b4fd4d
Ethan Li <fourdimentional31@gmail.com>
2026-02-26 19:17:51 -0500

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -4,20 +4,21 @@ figlet "ANCIENT VISION"
 echo "By Ethan Li"
 echo "MIT 2.0 License"
 cd
-wget https://artifact-alliance.vercel.app/install/vision.py -nv
-wget https://artifact-alliance.vercel.app/install/DRV8825.py -nv
-wget https://artifact-alliance.vercel.app/install/service.py -nv
-wget https://artifact-alliance.vercel.app/install/stop.py -nv
-wget https://artifact-alliance.vercel.app/install/service.sh -nv
-wget https://artifact-alliance.vercel.app/install/requierments.txt -nv
+wget https://artifact-alliance.vercel.app/install/vision.py -nv -q
+wget https://artifact-alliance.vercel.app/install/DRV8825.py -nv -q
+wget https://artifact-alliance.vercel.app/install/service.py -nv -q
+wget https://artifact-alliance.vercel.app/install/stop.py -nv -q
+wget https://artifact-alliance.vercel.app/install/service.sh -nv -q
+wget https://artifact-alliance.vercel.app/install/requierments.txt -nv -q
 chmod +x service.sh
 mkdir images
 cd images
-wget https://artifact-alliance.vercel.app/install/click.mp3 -nv
-wget https://artifact-alliance.vercel.app/install/button.png -nv
-wget https://artifact-alliance.vercel.app/install/GUI.png -nv
-wget https://artifact-alliance.vercel.app/install/title.png -nv
+wget https://artifact-alliance.vercel.app/install/click.mp3 -nv -q
+wget https://artifact-alliance.vercel.app/install/button.png -nv -q
+wget https://artifact-alliance.vercel.app/install/GUI.png -nv -q
+wget https://artifact-alliance.vercel.app/install/title.png -nv -q
 cd
-pip install -r requierments.txt --break-system-packages -qq
-echo "Installation complete! Run python service.py to start it now. The dashboard is hosted at http://localhost:1234"
+pip install -r -qq requierments.txt --break-system-packages --disable-pip-version-check
+echo "Installation complete! The dashboard is hosted at http://localhost:1234"
 echo "Also, set service.sh to run on startup!"
+python service.py
