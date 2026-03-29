ed91daa1455569d9d781d4b63a4322e0998651ab
Ethan Li <fourdimentional31@gmail.com>
2026-03-29 08:54:09 -0400

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -8,6 +8,12 @@ echo "MIT License"
 cd
 echo 'Installation Logs:'
 echo 'Checking for and removing old install files'
+figlet "Ancient Vision" >> install.log 2>&1
+echo "The future of archeological databases and scanning software" >> install.log 2>&1
+echo "By Ethan Li" >> install.log 2>&1
+echo "MIT License" >> install.log 2>&1
+echo 'Installation Logs:' >> install.log 2>&1
+echo 'Checking for and removing old install files' >> install.log 2>&1
 rm -f -- vision.py >> install.log 2>&1
 rm -f -- DRV8825.py >> install.log 2>&1
 rm -f -- service.py >> install.log 2>&1
@@ -16,6 +22,7 @@ rm -f -- service.sh >> install.log 2>&1
 rm -f -- requierments.txt >> install.log 2>&1
 rm -f -- index.html >> install.log 2>&1
 echo 'All old install files removed, pulling new ones'
+echo 'All old install files removed, pulling new ones' >> install.log 2>&1
 wget https://artifactalliance.org/install/vision.py -4 >> install.log 2>&1
 wget https://artifactalliance.org/install/DRV8825.py -4 >> install.log 2>&1
 wget https://artifactalliance.org/install/service.py -4 >> install.log 2>&1
@@ -23,6 +30,7 @@ wget https://artifactalliance.org/install/stop.py -4 >> install.log 2>&1
 wget https://artifactalliance.org/install/service.sh -4 >> install.log 2>&1
 wget https://artifactalliance.org/install/requierments.txt -4 >> install.log 2>&1
 wget https://artifactalliance.org/install/index.html -4 >> install.log 2>&1
+echo 'Main Files pulled; setting service.sh as executable' >> install.log 2>&1
 echo 'Main Files pulled; setting service.sh as executable'
 chmod +x service.sh
 rm images -r >> install.log 2>&1
@@ -32,12 +40,14 @@ rm -f -- button.png >> install.log 2>&1
 rm -f -- GUI.png >> install.log 2>&1
 rm -f -- title.png >> install.log 2>&1
 echo 'Downloading HTML5 Assets'
+echo 'Downloading HTML5 Assets' >> install.log 2>&1
 wget https://artifactalliance.org/install/click.mp3 -4 >> install.log 2>&1
 wget https://artifactalliance.org/install/button.png -4 >> install.log 2>&1
 wget https://artifactalliance.org/install/GUI.png -4 >> install.log 2>&1
 wget https://artifactalliance.org/install/title.png -4 >> install.log 2>&1
 cd
 echo 'Installing requiered python packages'
+echo 'Installing requiered python packages' >> install.log 2>&1
 pip install -r requierments.txt --break-system-packages --disable-pip-version-check >> install.log 2>&1
 echo 'Installing linux-wifi-hotspot' 
 rm linux-wifi-hotspot -r >> install.log 2>&1
@@ -49,7 +59,8 @@ make >> install.log 2>&1
 sudo make install >> install.log 2>&1
 systemctl enable create_ap >> install.log 2>&1
 cd
-echo "Installation complete! The dashboard is hosted at http://localhost:1234"
+echo "Installation complete! You can find the logs at install.log"
+echo "The dashboard is hosted at http://localhost:1234"
 echo "Set service.sh to run on startup!"
 echo "Flask App Logs:"
-python service.py || python3 service.py
+python service.py || python3 service.py > flask.log
